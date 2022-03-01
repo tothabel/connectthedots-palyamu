20d376c8c9a5a79171be8a1187c79f7e8465ee45
tothabel <83927032+tothabel@users.noreply.github.com>
2022-03-01 20:25:29 +0100

    Delete tutorial.js

--- tutorial.js
+++
@@ -1,8 +0,0 @@
-const tutorial_container = document.getElementById('tutorial_container')
-const bezaras = document.getElementById('bezaras')
-const tovabb = document.getElementById('tovabb')
-
-document.getElementById('bezaras').addEventListener('click', 
-function() {
-    document.getElementById('tutorial_container').remove();
-});
