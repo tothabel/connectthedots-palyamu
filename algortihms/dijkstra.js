75fd9786ab1eb4aa51ab6178efe13a8420e9ea75
tothabel <83927032+tothabel@users.noreply.github.com>
2022-03-01 20:29:15 +0100

    Update dijkstra.js

--- algortihms/dijkstra.js
+++ algortihms/dijkstra.js
@@ -1,5 +1,58 @@
+let distances = new Array;
 let dijkstraOrder = [];
+let dijkstraParent = []; //i is the parent of dijkstraParent[i]
+
+function minDistance(dist, vis){
+	
+	// Initialize min value
+	let min = Number.MAX_VALUE;
+	let min_index = -1;
+	
+	for(let v = 0; v < nodeNum; v++){
+		if (vis[v] == false && dist[v] <= min){
+			min = dist[v];
+			min_index = v;
+		}
+	}
+	return min_index;
+}
 
 function dijkstra(){
-    
+	let dist = new Array(nodeNum);
+	let vis = new Array(nodeNum);
+	dijkstraParent = new Array(nodeNum);
+	dijkstraOrder = new Array;
+
+	dijkstraParent[Startid] = -1;
+	
+
+	for(let i = 0; i < nodeNum; i++){
+		dist[i] = Number.MAX_VALUE;
+		vis[i] = false;
+		dijkstraParent.push(-1)
+	}
+	
+	dist[Startid-1] = 0;
+	
+	for(let count = 0; count < nodeNum; count++){
+		let u = minDistance(dist, vis);
+		
+		vis[u] = true;
+
+        dijkstraOrder.push(u);
+		
+		for(let v = 0; v < nodeNum; v++){
+			
+			if (!vis[v] && weights[u][v] != 0 &&
+				dist[u] != Number.MAX_VALUE &&
+				dist[u] + weights[u][v] < dist[v])
+            {
+				dist[v] = dist[u] + weights[u][v];
+				dijkstraParent[v] = u;
+			}
+		}
+
+	}
+
+	console.log(dist[Endid-1]);
 }
